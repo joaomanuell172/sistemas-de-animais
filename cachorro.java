00ee037bc1bf7abaae4ec0115ea6cb7aa4a98356
joão manuel <joaomanuellapa172@gmail.com>
2026-04-27 14:55:09 -0300

    Remove overload comment from emitirSom method
    Removed comment about method overloading in emitirSom.

--- cachorro.java
+++ cachorro.java
@@ -4,13 +4,11 @@ public class Cachorro extends Animal {
         super(nome);
     }
 
-    // Sobrescrita (Override)
     @Override
     public void emitirSom() {
         System.out.println(nome + " diz: Au au!");
     }
 
-    // Sobrecarga (Overload)
     public void emitirSom(int vezes) {
         System.out.print(nome + " diz: ");
         for (int i = 0; i < vezes; i++) {
