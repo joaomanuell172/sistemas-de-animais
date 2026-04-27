13afdde28b8325be069fbd5b931356643fe76dae
joão manuel <joaomanuellapa172@gmail.com>
2026-04-27 14:55:26 -0300

    Update main2.java

--- main2.java
+++ main2.java
@@ -2,12 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Cachorro meuCachorro = new Cachorro("Rex");
         Gato meuGato = new Gato("Frajola");
-
-        // Testando Sobrescrita
+        
         meuCachorro.emitirSom();
         meuGato.emitirSom();
-
-        // Testando Sobrecarga no Cachorro
+        
         meuCachorro.emitirSom(3);
     }
 }
